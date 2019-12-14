5d35a3e8e8a6b1da5565fc3ea0ccbb7f135cd559
Leon Jamison <leonjamison@icloud.com>
2019-12-14 19:05:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+# expect(rows[2]).to eq("   | X |   ")
+# board[8] = "X"
+display_board(board)
